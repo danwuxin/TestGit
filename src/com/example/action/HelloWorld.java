45b2dfed2b7ed54b8315e5e899f18968e921ad1b
danwuxin <danwuxin@126.com>
2018-12-27 10:25:56 +0800

    first commit

---
+++ src/com/example/action/HelloWorld.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: HelloWorld
+ * Author:   淡无心
+ * Date:     2018/12/27 10:22
+ * Description: 测试
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.example.action;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈测试〉
+ *
+ * @author 淡无心
+ * @create 2018/12/27
+ * @since 1.0.0
+ */
+public class HelloWorld {
+
+}
